534d7c3167b768523907b3d387614aa09dd6ae66
mohamed hassan <63217614+Moamed1010@users.noreply.github.com>
2026-07-11 03:23:33 +0300

    Implement Featured and Newest Books Cubits with state management

---
+++ lib/Features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:booklyapp/Features/home/domain/entities/book_entity.dart';
+import 'package:booklyapp/Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:meta/meta.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.fetchNewestBooksUseCase) : super(NewestBooksInitial());
+  final FetchNewestBooksUseCase fetchNewestBooksUseCase;
+  Future<void> fetchNewestBooks() async {
+    var result = await fetchNewestBooksUseCase.call();
+    result.fold(
+      (failure) {
+        emit(NewestBooksFailure(failure.message));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books));
+      },
+    );
+  }
+}
